01b4f1f246f971f8b01376bb66a9178a08d05526
eggyy1224 <eggyy1224@gmail.com>
2017-11-12 19:00:32 +0800

    join &quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,6 +49,34 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    unless current_user.is_member_of?(@group) 
+      current_user.join!(@group)
+      flash[:notice] = "加入本討論板成功"
+    else
+      flash[:warning] = "你已經是本討論板成員了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group) 
+      current_user.quit!(@group)
+      flash[:warning] = "你已退出本討論版"
+    else
+      flash[:warning] = "沒加入怎退出你搞笑嗎！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+
+
   private
 
   def find_group_and_check_permission
